4a59d26c875d5a57871ca6c3b52c96407cb98b2e
Ismail Ahmed <isahmed0149@gmail.com>
2022-01-18 22:00:56 -0500

    Starter code for parsing cli args

--- dotman/__main__.py
+++ dotman/__main__.py
@@ -1,14 +1,41 @@
 import sys
-
-
+import argparse
+import os
 
 
 def main():
 
+    username = os.getlogin()
+    if os.path.exists("/home/" + username + "/.dotman") == False:
+        print("~/.dotman folder does not exist setting up now...")
+        first_setup(username)
+    else:
+        print("does exist")
+
+
+    parser = argparse.ArgumentParser(description="Sync your dotfiles with one monolithic CLI tool")
+    group = parser.add_mutually_exclusive_group()
+    parser.add_argument("x", help="the base", type=int)
+    parser.add_argument("y", help="the exponent", type=int)
+    group.add_argument("-v", "--verbose", help="increase the verbosity of your output", action="count",
+                        default=0 )
+    group.add_argument("-q", "--quiet", action="store_true")
+    args = parser.parse_args()
+    answer = args.x ** args.y
+   
+    if args.quiet:
+        print(answer)
+    elif args.verbose:
+        print(f"{args.x} to the power of {args.y} equals {answer}")
+    else:
+        print(f"{args.x}^{args.y} == {answer} ")
+
+
+def first_setup(username: str):
     ...
 
-
-
+    os.mkdir("/home/" + username + "/.dotman")
+    
 
 
 

34c5b0300bdbfe9501d83b52bc3be9e9f5b1e02c
Ismail Ahmed <isahmed0149@gmail.com>
2022-05-08 21:08:16 -0400

    Restructure and refactor

--- main/dotman.py
+++ main/dotman.py
@@ -5,8 +5,9 @@ import re
 import json
 import git
 from typing import Optional
+
+from git.refs import remote
 from utils import utils
-from set_repo import set_repo
 
 def main():
 
@@ -17,7 +18,11 @@ def main():
 
 
     argument_parser = create_parser()
-    
+
+    if len(sys.argv) == 1:
+        print("usage")
+        sys.exit()
+
     args = argument_parser.parse_args()
     
     process_args(args)
@@ -25,13 +30,19 @@ def main():
 
 def create_parser():
     parser = argparse.ArgumentParser(description="Sync your dotfiles with one seamless CLI tool")
-    group = parser.add_mutually_exclusive_group()
+
     parser.add_argument("sync", help="sync your dotfiles with your current repo", nargs='?')
-    parser.add_argument("diff", help="Find the diff between local and remote versions of all dotfiles",
-                        nargs = '?')
+
+    # Changes need to be commited first 
+    # BUG: if just 'dotman diff' is run it gets assigned to sync since it's a positional argument
+    parser.add_argument("diff", help="Find the diff between local and remote versions of all dotfiles", const=1, nargs='?')
+
+    git_group = parser.add_mutually_exclusive_group()
+    git_group.add_argument("add", help="Stage dotfiles for commits", nargs='?')
+    git_group.add_argument("commit", help="Commit your staged changes", nargs='?')
+
     parser.add_argument("-s", "--setrepo", help="Set the git repo of your dotfiles")
     
-    group.add_argument("-q", "--quiet", action="store_true")
 
     return parser
 
@@ -39,18 +50,18 @@ def process_args(args: argparse.Namespace):
     setrepo = args.setrepo
     sync = args.sync
     diff = args.diff
+    breakpoint()
     if setrepo is not None: 
         is_setrepo_valid = validate_repo(setrepo)
         if is_setrepo_valid:
-            set_repo.set_repo(setrepo)
+            utils.set_repo(setrepo)
+            utils.clone(setrepo)
         else:
-            print("Repo url is not valid. Check for any typos or spelling mistakes")
-
-    
-
-
-
-
+            print("Repo url is not valid. Check for access rights and typos")
+    if sync is not None:
+        ...
+    if diff is not None:
+       utils.diff() 
 
 def firsttime_setup(username: str):
 
@@ -66,31 +77,33 @@ def firsttime_setup(username: str):
     with open(config_dir + "/config.json", 'w') as f:
         json.dump(data, f)
 
-
-
-
-
 def validate_repo(repo_url: str) -> Optional[bool]:
     """Return whether the given url points to an existing git
     repository 
-
     """
     
     match = re.match(r"((git|ssh|http(s)?)|(git@[\w\.]+))(:(//)?)([\w\.@\:/\-~]+)(\.git)?(/)?", repo_url)
 
-    # Now I will check if the url points to a public git repo
+    # Checks if the url points to a public git repo
+    if match is not None:
+        return exists(repo_url)
+    else:
+        return False
 
 
 
-    return match is not None
-
-def lsremote(url: str):
+def exists(url: str):
+    remote_refs = {}
     g = git.cmd.Git()
+    try:
+        refs = g.ls_remote(url).split('\n')
+        for ref in refs:
+            hash_ref_list = ref.split('\t')
+            remote_refs[hash_ref_list[1]] = hash_ref_list[0]
+        return remote_refs != {}
+    except (git.exc.GitCommandError):
+        print("Cannot access repo pointed to by url or repo may not exist...")
 
-    try g.ls_remote(url):
-        return True
-
-    
 
 
 def touch(path: str):
--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,71 @@
 from git.repo import Repo
 
+from git.repo import Repo
+import git
+import os
+import json
+import getpass
 
+def set_repo(url: str):
+
+    """
+    Set the repo for the dotfiles in the json config file
+
+    Precondition:
+        - The config file is created with the proper format
+        - The url has been validated as repo for a git repository
+
+    """
+    username = os.getlogin()
+    config_path = "/home/" + username + "/.config/dotman/config.json"
+    
+    with open(config_path, "r") as config_file:
+        data = json.load(config_file)
+
+    data['url'] = url
+    data['system_path'] = config_path
+
+    with open(config_path, "w") as config_file:
+        json.dump(data, config_file)
+
+    
 
 def clone(url: str):
+    """Clone the git repo into the home directory
+    using the repo url
 
-    Repo.clone_from(url, "~/") 
+    Precondition:
+        - url points to a valid and existing git repo
+        - ~/.config/dotman exists
+
+    """
+    try:
+        Repo.clone_from(url, '/home/' + getpass.getuser() + '/.config/dotman/dotfiles')
+        link_files()
+    except git.exc.GitError:
+        print('ERROR! Repo does not exist!')
+
+def link_files():
+    ...
+    
+
+def diff():
+    breakpoint()
+    username = os.getlogin()
+    if os.path.exists("/home/" + username + "/.config/dotman") == False:
+       print("Repo is not set. Invalid usage") 
+    else:
+        repo = Repo("/home/" + username + "/.config/dotman/dotfiles")
+        branch = repo.active_branch.name
+        commit_current = repo.commit(branch)
+        commit_origin_current = repo.commit("origin/" + branch)
+        diff_index = commit_origin_current.diff(commit_current)
+        for diff_item in diff_index.iter_change_type('M'):
+            # TODO: Figure out a way to format diff better like in 'git diff'
+            print("A blob:\n{}".format(diff_item.a_blob.data_stream.read().decode('utf-8')))
+            print("B blob:\n{}".format(diff_item.b_blob.data_stream.read().decode('utf-8')))
+
+def sync():
+    ...
 
 
